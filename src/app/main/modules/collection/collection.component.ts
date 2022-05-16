e009858c308cdc4becc95ef56c8969fba14aea97
Julio Quintero <julioquintetro@gmail.com>
2022-05-15 21:40:27 -0500

    collection feature creation

--- src/app/main/modules/collection/collection.component.ts
+++ src/app/main/modules/collection/collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-collection',
@@ -7,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+  public readonly collection = new FormGroup({
+    title: new FormControl('', [
+      Validators.minLength(1),
+      Validators.maxLength(100),
+    ]),
+    description: new FormControl('', [
+      Validators.minLength(0),
+      Validators.maxLength(250),
+    ]),
+    images_ids: new FormControl([], [
+      Validators.required,
+    ]),
+  });
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  test(url) {
+    console.log(url)
+  }
+
+  send() {
+    console.log(this.collection.value);
+  }
+
+  loadData(event) {
+    console.log(event);
+  }
 
 }
